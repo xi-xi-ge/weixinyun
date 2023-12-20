2be7db1d8b3cab88fc391b8834c6b525e95a3f66
Steve <44634464+xi-xi-ge@users.noreply.github.com>
2023-12-20 16:40:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 
 const proxy = httpProxy.createProxyServer({});
-const targetServer = 'https://www.luban-tec.com';
+const targetServer = 'http://www.lubantec.com';
 
 const server = http.createServer((req, res) => {
-  // 处理微信小程序调用，获取微信 Open ID
-  if (req.url === '/api/wx_openid' && req.headers['x-wx-source']) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(req.headers['x-wx-openid']);
-    return;
+  if (req.method === 'POST') {
+    req.headers['content-type'] = 'application/json'; // 设置 Content-Type
   }
 
-  // 将请求转发到另一个代理服务器
   proxy.web(req, res, { target: targetServer }, (err) => {
     console.error('代理请求错误:', err);
     res.writeHead(500, { 'Content-Type': 'text/plain' });
